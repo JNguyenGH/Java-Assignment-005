d62da8724303a6e11112a3ad3c8dce94cecdc915
Jesse <nguyen.jesse0@gmail.com>
2022-10-02 22:54:53 -0700

    CIS - 12 Assignment 5

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -22,8 +22,10 @@ public class TempConversion {
     public static double convertK2C(double kelvin) {
         return kelvin - 273.15;
     }
-
-    public static double getTemp(String unit) {
+public static double convertK2F (double kelvin) {
+        return convertC2F(convertK2F(kelvin));
+}
+    public static double getTemp() {
         System.out.printf("%-40s : ", String.format("Please enter °%s temperature to convert", unit));
         return Double.parseDouble(input.nextLine());
     }
@@ -37,8 +39,59 @@ public class TempConversion {
     }
 
     public static void main(String[] args) {
-        while(true) {
+        String from;
+        while (true) {
+            System.out.println("Enter a temperature unit to convert from:");
+            from = getUnitChoice();
+            if (from.equals("Q") || from.equals("q")) {
+                break;
+                double fromValue = getTemp();
+                double to = 0.0;
+            }
+        }
+        System.out.println("Enter a temperature unit to convert to:");
+        String to = getUnitChoice();
+        if (to.equals("Q") || to.equals("q")) {
+            break;
+        }
+        System.out.println("Enter a double temperature value to convert:");
+        double convert = getTemp();
+        switch (from + to) {
+            case "K":
+                switch (to) {
+                    case "F" -> {
+                        to = convertK2F(from);
+                        System.out.println("K to F");
+                    }
+                    case "C" -> {
+                        to = convertK2C(from);
+                        System.out.println("K to C");
+                    }
+                }
+                break;
+            case "C":
+                switch (to) {
+                    case "F"->{
+                        to = convertC2F(from);
+                        System.out.println("C to F");
+                    }
+                    case "K"-> {
+                        to = convertC2K(from);
+                        System.out.println("C to K");
+                    }
+                }
+            case "F":
+                switch (to) {
+                    case "C"-> {
+                        to = convertK2C(from);
+                        System.out.println("F to C");
+                    }
+                    case "K"->{
+                        to = convertF2K(from);
+                        System.out.println("F to K");
+                    }
+                }
 
         }
-    }
+        System.out.println("Result 40.000000°C is 104.000000°F");}
 }
